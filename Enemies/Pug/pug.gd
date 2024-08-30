3b92cd1948c810853c1f50736d20dba45632facd
badorius <salvadormuntane@gmail.com>
2024-08-30 23:42:35 +0200

    Instantiate pugs on lvl1 and start Pug IA movement

--- Enemies/Pug/pug.gd
+++ Enemies/Pug/pug.gd
@@ -1,34 +1,52 @@
 extends CharacterBody2D
 
 
-const SPEED = 50.0
+const SPEED = 30.0
 const JUMP_VELOCITY = -300.0
 
+ 
+
 #Start random direction 
-@onready var rndx = randi() % 2
-@onready var rndy = randi() % 2
+@onready var random = RandomNumberGenerator.new()
+
+@export var direction = 0
+@export var rect : Vector2
+
 
 func _ready() -> void:
-	pass
+	RandomNumberGenerator.new()
+	random_direction()
+	
+func random_direction():
+	var randomnumber = random.randi_range(-1, 1)
+	if randomnumber == 0:
+		random_direction()
+	else:
+		direction = randomnumber
 
 func _physics_process(delta: float) -> void:
-	print (rndx)
-	print (rndy)
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# If Pug is on bottom, jump! needs to improve.
+	if position.y >= 88 and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = rndx
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if global_position.x >= rect.x - 23:
+		direction = direction * -1
+		print(global_position.x)
+		
+	if global_position.x <= (rect.x - 23) * -1:
+		direction = direction * -1
+		print(global_position.x)
+	
+		#Uncoment to stop
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	velocity.x = direction * SPEED
+
 
 	move_and_slide()
--- Levels/Level1/level_1.gd
+++ Levels/Level1/level_1.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+#Var to public view viewport size rectangle from camera
+@export var rect : Vector2
+
+#Var enemies to instantiate on level:
+@export var num_pug = 5
+@export var pug_scene: PackedScene = preload("res://Enemies/Pug/pug.tscn")
+
+# Var to get spawn_timer
+@onready var spawn_timer = $Timer
+
+# Var to count how many enemies has been spawned:
+var pug_spawned = 0
 
 #MUSIC VAR
 @onready var BgMusic = $BGMusicLVL1
@@ -7,8 +19,45 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
+	randomize() # Neede to  get random spawn each time
+	spawn_timer.connect("timeout", Callable(self, "_on_SpawnTimer_timeout"))
+	schedule_next_spawn() # Call function
+	#Assign viewport rect value / 2.0 in order to work between 1 vs -1 
+	rect = get_viewport_rect().size / 2.0
 	BgMusic.play()
-	print("music")
+	
+	
+func _on_SpawnTimer_timeout():
+	if pug_spawned < num_pug:
+		spawn_pug()
+		pug_spawned += 1
+		schedule_next_spawn()
+	else:
+		spawn_timer.stop()
+
+func schedule_next_spawn():
+	var random_interval = randi_range(1.0, 5.0)  # Schedule next between 1, 3 seconds
+	spawn_timer.wait_time = random_interval
+	spawn_timer.start()
+
+func spawn_pug():
+	var pug_instance = pug_scene.instantiate()
+	pug_instance.position = get_random_position()
+	add_child(pug_instance)
+	#Pass rect value to pug in order to get movement inteligence depending of sice window
+	pug_instance.rect = rect
+	print("Pug instanciado en posición: ", pug_instance.position)
+
+func get_random_position() -> Vector2:
+	#Randomize x position from rect value
+	var random_x = randf_range(rect.x * -1, rect.x)
+	#Uncoment this line if you want to randomize y
+	#var random_y = randf_range(rect.y * -1, rect.y)
+	#Fix y position:
+	var random_y = -70
+
+	
+	return Vector2(random_x, random_y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
