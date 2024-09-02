c87d5ce876d93673ab1d5946f29e3f068baacea2
badorius <salvadormuntane@gmail.com>
2024-09-02 21:58:35 +0200

    improve animation bobble and pug

--- ShotBlood/shot_blood.gd
+++ ShotBlood/shot_blood.gd
@@ -4,10 +4,13 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 @export var direction : int = 1
+@export var ReadyToExplode : int = 0
+
 
 var state_machine
 
-@onready var bubble_sound = $BubblePop
+@onready var bubble_sound_pop = $BubblePop
+@onready var bubble_sound_explode = $BubbleExplode
 
 
 func _ready() -> void:
@@ -20,20 +23,30 @@ func _ready() -> void:
 		if direction == -1:
 			get_node( "ShotBloodSprite" ).set_flip_h( true )
 		
-		bubble_sound.play()
+		bubble_sound_pop.play()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+			
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 		velocity.x = direction * SPEED
 
 	move_and_slide()
 	
+func explode():
+	state_machine.travel('Explode')
+
+	
 #SIGNALS START HERE
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemies"):
 		print("Collide")
+	else:
+		ReadyToExplode += 1
+		if ReadyToExplode >= 2:
+			explode()
+			
