0c934b6d21cce8094cd044cb25e3713b359c0d12
badorius <salvadormuntane@gmail.com>
2024-09-08 17:30:24 +0200

    Add boot rom

--- Levels/Boot/boot.gd
+++ Levels/Boot/boot.gd
@@ -1,22 +1,89 @@
 extends Node2D
 
 var characters = 'abcdefghijklmnopqrstuvwxyz'
-@export var new_string : String
+@onready var timer = $Timer
+@export var stage : int
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_string = print_random_chars(characters, 16)
-	print(new_string)
-	$Label.text=new_string
+	#Set all labels as no visible, will be visible on each stage
+	$LabelRndChars.visible = false
+	$LabelRomCheck.visible = false
+	$TileMapLayer.visible = false
+	
+	randomize()
+	timer.start()
+	stage = 1
+	$LabelRndChars.text=get_rnd_chars(characters, 20)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	new_string = print_random_chars(characters, 16)
-	$Label.text=new_string + "\n"
+	print(stage)
+	match stage:
+		1:
+			print_rnd_chars()
+		2:
+			RomCheck()
+		3:
+			showgrid()
 
-func print_random_chars(chars, length):
+	
+func print_rnd_chars():
+	$LabelRndChars.visible = true
+	if $LabelRndChars.text.length() < 400:
+		$LabelRndChars.text+=get_rnd_chars(characters, 20)
+
+	else:
+		var random_colour = Color(randf(), randf(), randf())
+		$LabelRndChars.label_settings.font_color = random_colour
+		$LabelRndChars.text=get_rnd_chars(characters, 20)
+
+	
+func get_rnd_chars(chars, length):
 	var word: String
 	var n_char = len(chars)
 	for i in range(length):
 		word += chars[randi()% n_char]
-	return word
+	return word + "\n"
+
+func RomCheck():
+	$LabelRndChars.visible = false
+	$LabelRomCheck.visible = true
+	
+	$LabelRomCheck.label_settings.font_color = change_color()
+	$LabelRomCheck.text = "RAM CHECK...\n"
+	await get_tree().create_timer(0.5).timeout
+	$LabelRomCheck.label_settings.font_color = change_color()
+	$LabelRomCheck.text += "RAM OK\n"
+	await get_tree().create_timer(0.5).timeout
+	$LabelRomCheck.label_settings.font_color = change_color()
+	$LabelRomCheck.text += "ROM CHECK...\n"
+	await get_tree().create_timer(0.5).timeout
+	$LabelRomCheck.label_settings.font_color = change_color()
+	$LabelRomCheck.text += "ROM OK\n"
+	await get_tree().create_timer(0.5).timeout
+	stage +=1
+
+
+func change_color():
+	var random_colour = Color(randf(), randf(), randf())
+	return random_colour
+
+
+func showgrid():
+	$LabelRndChars.visible = false
+	$LabelRomCheck.visible = false
+	$TileMapLayer.visible = true
+	
+func quitboot():
+	get_tree().quit()
+
+	
+
+func _on_time_rnd_chars_timeout() -> void:
+	if stage < 3:
+		stage += 1
+	else:
+		quitboot()
+	 
