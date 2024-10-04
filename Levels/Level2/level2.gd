468ecc12d20f0a3aa48c9b5d5937d05c807121d0
badorius <salvadormuntane@gmail.com>
2024-10-04 12:41:34 +0200

    Test pixel issue

--- Levels/Level2/level2.gd
+++ Levels/Level2/level2.gd
@@ -8,18 +8,7 @@ extends Node2D
 #MUSIC VAR
 @onready var BgMusic = $BGMusicLVL1
 
-@onready var Cauldron1 : CharacterBody2D = get_node("../Cauldron1")
-@onready var Cauldron2 : CharacterBody2D = get_node("../Cauldron2")
-@onready var Cauldron3 : CharacterBody2D = get_node("../Cauldron3")
 
-@onready var Crucifix1_floor1 : CharacterBody2D = get_node("../Crucifix1_floor2")
-@onready var Crucifix2_floor1 : CharacterBody2D = get_node("../Crucifix2_floor2")
-@onready var Crucifix1_floor2 : CharacterBody2D = get_node("../Crucifix1_floor2")
-@onready var Crucifix2_floor2 : CharacterBody2D = get_node("../Crucifix2_floor2")
-@onready var Crucifix3_floor2 : CharacterBody2D = get_node("../Crucifix3_floor2")
-@onready var Crucifix4_floor2 : CharacterBody2D = get_node("../Crucifix4_floor2")
-@onready var Crucifix1_floor3 : CharacterBody2D = get_node("../Crucifix1_floor3")
-@onready var Crucifix2_floor3 : CharacterBody2D = get_node("../Crucifix2_floor3")
 
 func _ready():
 	BgMusic.play()
